7630a98b3984f056e6ff59484f1761abdcc95962
Jamshid Dostonov <Dostonov888@yandex.ru>
2021-02-04 04:29:31 +0300

    Добавил д.з от 3 до 5 того урока

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 let num = 266219;
 
 num = Array.from(num.toString()).map(Number);
-console.log(num = num.reduce((res, item) => item * res, 1));
-console.log(num = num **= 3);
+console.log(num);
+console.log(num = num.reduce((res, item) => res * item, 1));
+console.log(num **= 3);
+console.log(num);
 console.log(String(num).slice(0, 2));
+
+
+
+const getDayName = (number, lang) => ({
+    en: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    ru: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+})[lang][number % 7];
+
+console.log(getDayName(3, 'ru'));
+console.log(getDayName(6, 'en'));
+
+
+
+
+function test(str) {
+    if (typeof str !== 'string') {
+        alert('аргумент не строка');
+        return;
+    }
+
+    str = str.trim();
+    return str.length > 50 ? str.slice(0, 50) + '...' : str;
+};
+let arr = ['1991', '4994', '1996', '1999', '2000', '2002', '2020'];
+arr.forEach((item) => {
+    if (item.startsWith('2') || item.startsWith('4')) {
+        console.log(item);
+    }
+});
